9a1e97d455e9587259893e0157b486ab075dba42
JARP <andersonandy046@gmail.com>
2024-11-22 07:25:23 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Admin Login</title>
   <script src="https://cdn.tailwindcss.com"></script>
+  <link rel="icon" type="image/png" href="assets/logo1.png">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/AlertifyJS/1.13.1/css/alertify.css" integrity="sha512-MpdEaY2YQ3EokN6lCD6bnWMl5Gwk7RjBbpKLovlrH6X+DRokrPRAF3zQJl1hZUiLXfo2e9MrOt+udOnHCAmi5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/AlertifyJS/1.13.1/alertify.min.js" integrity="sha512-JnjG+Wt53GspUQXQhc+c4j8SBERsgJAoHeehagKHlxQN+MtCCmFDghX9/AcbkkNRZptyZU4zC8utK59M5L45Iw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
@@ -22,7 +23,7 @@
 
     <!-- Logo -->
     <div class="flex justify-center mb-6">
-      <img src="assets/logo1.png" alt="Company Logo" class="w-24 h-24 object-contain">
+      <img src="assets/logo1.png" alt="Company Logo" class="w-30 h-30 object-contain">
     </div>
 
     <h2 class="text-2xl font-bold text-center text-gray-800 mb-6">Admin Login</h2>

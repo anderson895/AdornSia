91b1a16b66396e619fd55063bf2fce910e9fa6c2
JARP <andersonandy046@gmail.com>
2024-11-22 07:32:28 +0800

    Update login.php

--- login.php
+++ login.php
@@ -16,7 +16,7 @@
         </div>
      </div>
 
-    <h2 class="text-2xl font-bold text-center text-gray-800 mb-6">ADORN</h2>
+    <h2 class="text-2xl font-bold text-center text-gray-800 mb-6">ADORN SIA</h2>
     
     <form id="frmLogin" class="space-y-6">
       <div>

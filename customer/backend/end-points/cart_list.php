550e3c2dea38771c0b51a67752701d8a67c12f0b
JARP <andersonandy046@gmail.com>
2024-11-21 22:44:53 +0800

    responsiveness

--- customer/backend/end-points/cart_list.php
+++ customer/backend/end-points/cart_list.php
@@ -5,12 +5,9 @@ $totalItems = count($getCartlist);
 $subTotal = 0;
 $totalSavings = 0;
 ?>
-
 <div class="flex flex-col lg:flex-row gap-8">
     <!-- Left Section: Cart Items -->
     <div class="flex-grow">
-       
-
         <?php if ($totalItems == 0): ?>
             <!-- Display this if the cart is empty -->
             <p class="text-lg text-center text-gray-500">Your cart is empty. Add some items to get started!</p>
@@ -29,7 +26,6 @@ $totalSavings = 0;
                     $totalSavings += ($cart['prod_currprice'] - $discounted_price) * $cart['cart_Qty'];
                 }
 
-
                 $originalPrice = $cart['prod_currprice'];
 
                 if ($cart['prod_promo_id']) {
@@ -37,64 +33,58 @@ $totalSavings = 0;
                 } else {
                     $price = number_format($cart['prod_currprice'] * $cart['cart_Qty'], 2);
                 }
-
-
-                
-
             ?>
             <!-- Product Item -->
-            <div class="flex items-center border-t border-gray-200 pt-6 relative">
+            <div class="flex flex-col lg:flex-row items-center border-t border-gray-200 pt-6 relative lg:space-x-6">
                 <!-- Delete Button (X) -->
                 <button class="absolute top-0 right-0 text-red-600 text-lg p-1 hover:bg-gray-200 rounded-full TogglerRemoveItem" 
-                        data-cart_id=<?=$cart['cart_prod_id']?>
-                        data-size='<?=$cart['cart_prod_size']?>'
-                        >
+                        data-cart_id="<?=$cart['cart_prod_id']?>"
+                        data-size='<?=$cart['cart_prod_size']?>'>
                     <span class="material-icons">close</span>
                 </button>
 
                 <input type="checkbox" 
-       class="product-checkbox mr-4 text-red-500" 
-       data-product-id="<?=$cart['cart_prod_id']?>" 
-       data-Originalprice="<?=$originalPrice?>". 
-       data-price=<?=$price?>
-       data-size="<?=$cart['cart_prod_size']?>" 
-       data-qty="<?=$cart['cart_Qty']?>" 
-       data-promoName="<?=$cart['promo_name']?>" 
-       data-promoRate="<?=$cart['promo_rate']?>">
+                    class="product-checkbox mr-4 text-red-500" 
+                    data-product-id="<?=$cart['cart_prod_id']?>" 
+                    data-Originalprice="<?=$originalPrice?>" 
+                    data-price="<?=$price?>"
+                    data-size="<?=$cart['cart_prod_size']?>" 
+                    data-qty="<?=$cart['cart_Qty']?>" 
+                    data-promoName="<?=$cart['promo_name']?>" 
+                    data-promoRate="<?=$cart['promo_rate']?>">
 
+                <img src="../upload/<?=$cart['prod_image']?>" alt="Product Image" class="w-20 h-20 object-cover rounded-md shadow-lg mr-6 mb-4 lg:mb-0">
 
-
-       <img src="../upload/<?=$cart['prod_image']?>" alt="Product Image" class="w-20 h-20 object-cover rounded-md shadow-lg mr-6">
                 <div class="flex-grow">
                     <h4 class="font-semibold text-gray-900"><?=$cart['prod_name']?></h4>
                     <p class="text-sm text-gray-600"><?= substr($cart['prod_description'], 0, 50) ?></p>
-                    <p class="text-sm text-gray-600 ">Size: <?=$cart['cart_prod_size']?></p>
-                    <p class="text-sm text-gray-600 ">Original price: <?=$cart['prod_currprice']?></p>
-                    <p class="text-sm text-gray-600 ">Avilable stocks: <?=$cart['product_stocks']?></p>
+                    <p class="text-sm text-gray-600">Size: <?=$cart['cart_prod_size']?></p>
+                    <p class="text-sm text-gray-600">Original price: <?=$cart['prod_currprice']?></p>
+                    <p class="text-sm text-gray-600">Available stocks: <?=$cart['product_stocks']?></p>
 
                     <!-- Quantity Input with Buttons -->
                     <div class="flex items-center space-x-2 mt-2">
                         <button class="togglerMinus w-8 h-8 bg-gray-200 text-gray-800 rounded-md flex items-center justify-center hover:bg-gray-300 focus:outline-none"
-                        data-user_id='<?=$cart['cart_user_id']?>'
-                        data-product_id='<?=$cart['cart_prod_id']?>'
-                        data-cart_prod_size='<?=$cart['cart_prod_size']?>'>
+                                data-user_id='<?=$cart['cart_user_id']?>'
+                                data-product_id='<?=$cart['cart_prod_id']?>'
+                                data-cart_prod_size='<?=$cart['cart_prod_size']?>'>
                             <span class="material-icons text-sm">remove</span>
                         </button>
                         <input readonly type="number" value="<?=$cart['cart_Qty']?>" class="w-16 h-8 text-center border border-gray-300 rounded-md px-2 py-1 text-sm" placeholder="Qty: 1" min="1" />
                         <button class="togglerAdd w-8 h-8 bg-gray-200 text-gray-800 rounded-md flex items-center justify-center hover:bg-gray-300 focus:outline-none"
-                        data-user_id='<?=$cart['cart_user_id']?>'
-                        data-product_id='<?=$cart['cart_prod_id']?>'
-                        data-cart_prod_size='<?=$cart['cart_prod_size']?>'>
+                                data-user_id='<?=$cart['cart_user_id']?>'
+                                data-product_id='<?=$cart['cart_prod_id']?>'
+                                data-cart_prod_size='<?=$cart['cart_prod_size']?>'>
                             <span class="material-icons text-sm">add</span>
                         </button>
                     </div>
                 </div>
                 <div class="text-right">
                     <?php if ($cart['prod_promo_id']): ?>
-                        <p class="text-red-600 font-semibold ">Php <span class='itemPrice'></span><?=number_format(($cart['prod_currprice']-($cart['prod_currprice']-$discounted_price))*$cart['cart_Qty'])?></p>
-                        <p class="text-xs line-through text-gray-400 has-promo">Php <?=number_format($cart['prod_currprice']*$cart['cart_Qty'])?> - <?=($cart['promo_rate']*100)?>%</p>
+                        <p class="text-red-600 font-semibold">Php <?=number_format(($cart['prod_currprice'] - ($cart['prod_currprice'] - $discounted_price)) * $cart['cart_Qty'], 2)?></p>
+                        <p class="text-xs line-through text-gray-400 has-promo">Php <?=number_format($cart['prod_currprice'] * $cart['cart_Qty'])?> - <?=($cart['promo_rate'] * 100)?>%</p>
                     <?php else: ?>
-                        <p class="text-red-600 font-semibold itemPrice">Php <?=number_format($cart['prod_currprice']*$cart['cart_Qty'], 2)?></p>
+                        <p class="text-red-600 font-semibold itemPrice">Php <?=number_format($cart['prod_currprice'] * $cart['cart_Qty'], 2)?></p>
                     <?php endif; ?>
                 </div>
             </div>
@@ -102,3 +92,4 @@ $totalSavings = 0;
         <?php endif; ?>
     </div>
 </div>
+

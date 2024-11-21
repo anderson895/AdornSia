df321831230867a2960b42459b49b21f7d452a29
JARP <andersonandy046@gmail.com>
2024-11-21 23:01:42 +0800

    responsiveness

--- admin/orderlist.php
+++ admin/orderlist.php
@@ -25,7 +25,7 @@ if (isset($_GET['step'])) {
 
 
   <!-- Tabs -->
-  <div class="flex justify-center items-center flex-wrap space-x-0 space-y-2 md:space-y-0 md:space-x-4 border-b mb-6">
+  <div class="flex justify-center space-x-4 space-y-2 md:space-y-0 md:space-x-4 border-b mb-6 overflow-x-auto whitespace-nowrap">
       <a href="?step=Pending" class="py-2 px-4 text-gray-600 hover:text-red-500 <?= ($defaultStep == 'Pending' ? 'border-b-2 border-red-500 text-red-500' : '') ?>">Pending</a>
       <a href="?step=Accept" class="py-2 px-4 text-gray-600 hover:text-red-500 <?= ($defaultStep == 'Accept' ? 'border-b-2 border-red-500 text-red-500' : '') ?>">Accept</a>
       <a href="?step=Shipped" class="py-2 px-4 text-gray-600 hover:text-red-500 <?= ($defaultStep == 'Shipped' ? 'border-b-2 border-red-500 text-red-500' : '') ?>">Shipped</a>

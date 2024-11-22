497c54051912e7f2613a400ffa42c8511b0a4b4c
JARP <andersonandy046@gmail.com>
2024-11-22 17:42:14 +0800

    Update top5bestSelling.php

--- admin/backend/end-points/top5bestSelling.php
+++ admin/backend/end-points/top5bestSelling.php
@@ -5,16 +5,13 @@ $db = new global_class();
 $orders = $db->top5bestSelling();
 
 if ($orders) {
-    echo "<h1>Top 5 Best-Selling Products</h1>";
-    echo "<table border='1'>";
-    echo "<tr><th>Rank</th><th>Product Name</th><th>Quantity Sold</th></tr>";
     
     $rank = 1;
     foreach ($orders as $order) {
         echo "<tr>";
         echo "<td>" . $rank . "</td>";
         echo "<td>" . htmlspecialchars($order['prod_name']) . "</td>";
-        echo "<td>" . htmlspecialchars($order['total_quantity_sold']) . "</td>";
+        echo "<td>" . htmlspecialchars($order['prod_image']) . "</td>";
         echo "</tr>";
         $rank++;
     }

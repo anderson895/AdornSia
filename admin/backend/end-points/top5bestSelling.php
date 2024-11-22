00ffdd29cbc41edd861535f288f35917a5a62d51
JARP <andersonandy046@gmail.com>
2024-11-22 17:30:07 +0800

    Update top5bestSelling.php

--- admin/backend/end-points/top5bestSelling.php
+++ admin/backend/end-points/top5bestSelling.php
@@ -6,9 +6,14 @@ $db = new global_class();
 $orders = $db->top5bestSelling();
 
 header('Content-Type: application/json');
+
 if ($orders) {
     echo json_encode($orders);
 } else {
+    // Log the error message to a file
+    error_log('Error in top5bestSelling: ' . $db->conn->error);
+
+    // Return the error message to the client
     echo json_encode(['error' => 'No data available or an error occurred']);
 }
 ?>

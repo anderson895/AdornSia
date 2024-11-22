d281bd6942162a85531da54df5aca2e3d4ee8328
JARP <andersonandy046@gmail.com>
2024-11-22 17:38:41 +0800

    Update top5bestSelling.js

--- admin/js/top5bestSelling.js
+++ admin/js/top5bestSelling.js
@@ -6,8 +6,7 @@ $.ajax({
        
     },
     error: function(xhr, status, error) {
-        console.error('AJAX Error:', status, error);  // Log error details
-        console.log('Response Text:', xhr.responseText); // Log the raw response
+       
         $('#bestSellingProducts').html('<li class="text-sm text-red-600">An error occurred while fetching data.</li>');
     }
 });

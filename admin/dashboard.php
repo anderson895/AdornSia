73e45d977f5bc95e1f4b516ed8d1f14a24179c2e
JARP <andersonandy046@gmail.com>
2024-11-22 21:12:55 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -41,6 +41,10 @@
 
 
 
+
+
+<!-- Horizontal Scrollable Cards for Product Categories -->
+<div class="mt-8 px-4">
     <div class="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-6">
         
         
@@ -62,6 +66,8 @@
 
         
     </div>
+</div>
+
 
 
 

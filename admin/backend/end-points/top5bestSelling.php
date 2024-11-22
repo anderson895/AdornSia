b83063c037cef3eb910b2aef02e7a71194c8f13b
JARP <andersonandy046@gmail.com>
2024-11-22 18:17:34 +0800

    Update top5bestSelling.php

--- admin/backend/end-points/top5bestSelling.php
+++ admin/backend/end-points/top5bestSelling.php
@@ -5,18 +5,22 @@ $db = new global_class();
 $orders = $db->top5bestSelling();
 
 if ($orders) {
-    $response = "<ul class='list-disc pl-5'>";  // Start list
+    $response = "<ul class='space-y-4'>";  // Add some spacing between list items
     $rank = 1;
     foreach ($orders as $order) {
         $response .= '
-            <li class="text-sm text-gray-600 mb-2">
-                <strong>' . $rank . '. ' . $order['prod_name'] . '</strong><br>
-                <img src="../../../upload/' . $order['prod_image'] . '" alt="' . $order['prod_name'] . '" class="w-16 h-16 object-cover mt-2">
+            <li class="flex items-center space-x-4">
+                <div class="w-16 h-16">
+                    <img src="../../../upload/' . $order['prod_image'] . '" alt="' . $order['prod_name'] . '" class="w-full h-full object-cover rounded-lg">
+                </div>
+                <div class="flex-1">
+                    <h4 class="text-gray-700 font-semibold">' . $rank . '. ' . $order['prod_name'] . '</h4>
+                </div>
             </li>
         ';
         $rank++;
     }
-    $response .= "</ul>";  // End list
+    $response .= "</ul>";  // Close the list
     echo $response;
 } else {
     echo "<p class='text-gray-500'>No data available or an error occurred.</p>";

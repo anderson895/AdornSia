280493f47b2b420f75c84635b1df5e63ec503d71
JARP <andersonandy046@gmail.com>
2024-11-22 21:26:03 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -70,7 +70,7 @@
 
 <!-- Horizontal Scrollable Cards for Product Categories -->
 <div class="mt-8">
-    <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6 w-full mx-auto justify-center">
+    <div class="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 gap-6 w-full mx-auto justify-center">
         <!-- Best Selling Products Card -->
         <div class="bg-white p-6 rounded-lg shadow-lg w-full" id="bestSellingProducts">
         </div>

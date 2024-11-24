d3dcd65bfd38a31306fa637eebbf139bb0a52323
JARP <andersonandy046@gmail.com>
2024-11-24 13:55:36 +0800

    add promotion

--- admin/components/header.php
+++ admin/components/header.php
@@ -77,7 +77,7 @@ if (isset($_SESSION['admin_id'])) {
       </a>
 
 
-      <a href="{{url_for('admin_student_progress')}}" class="flex items-center lg:justify-start  space-x-3 text-gray-600 hover:text-blue-500 hover:bg-gray-100 px-4 py-2 rounded-md transition-all duration-300">
+      <a href="promotion.php" class="flex items-center lg:justify-start  space-x-3 text-gray-600 hover:text-blue-500 hover:bg-gray-100 px-4 py-2 rounded-md transition-all duration-300">
           <span class="material-icons">discount</span>
           <span>Marketing and Promotions</span>
       </a>
---
+++ admin/promotion.php
@@ -0,0 +1,38 @@
+<?php 
+include "components/header.php";
+?>
+
+<div class="flex justify-between items-center bg-white p-4 mb-6 rounded-md shadow-md">
+    <h2 class="text-lg font-semibold text-gray-700">Customer</h2>
+    <div class="w-10 h-10 bg-gray-300 rounded-full flex items-center justify-center text-lg font-bold text-white">
+        <?php
+        echo substr(ucfirst($_SESSION['admin_username']), 0, 1);
+        ?>
+    </div>
+</div>
+
+<!-- Card for Table -->
+<div class="bg-white rounded-lg shadow-lg p-6">
+    <h3 class="text-xl font-semibold text-gray-700 mb-4">Customer List</h3>
+
+    <!-- Table Wrapper for Responsiveness -->
+    <div class="overflow-x-auto">
+        <table id="userTable" class="display table-auto w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="bg-gray-100 text-gray-700">
+                <tr>
+                    <th class="p-2">ID</th>
+                    <th class="p-2">Fullname</th>
+                    <th class="p-2">Email</th>
+                    <th class="p-2">Phone</th>
+                    <th class="p-2">Status</th>
+                    <th class="p-2">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php include "backend/end-points/customers_list.php"; ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<?php include "components/footer.php";?>

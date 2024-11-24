232a7c1f2ffb92c0680926842b982a35e1ad5a77
JARP <andersonandy046@gmail.com>
2024-11-24 14:11:32 +0800

    marketing  promotional

--- admin/backend/end-points/promotional_list.php
+++ admin/backend/end-points/promotional_list.php
@@ -15,7 +15,6 @@ if ($fetch_all_promotion): ?>
         </ul>
     </div>
 
-    <!-- Table displaying the promotions -->
   
             <?php foreach ($fetch_all_promotion as $promotion): 
                 // Get the current date in Asia/Manila timezone
@@ -30,14 +29,18 @@ if ($fetch_all_promotion): ?>
                     <td class="p-2"><?= $promotion['promo_name']; ?></td>
                     <td class="p-2"><?= $promotion['promo_description']; ?></td>
                     <td class="p-2"><?= ($promotion['promo_rate'] * 100) / 2; ?>%</td>
-                    <td class="p-2"><?= $promotion['promo_expiration']; ?></td>
+
+                    <!-- Conditional font color for promo expiration -->
+                    <td class="p-2" style="color: <?= $is_expired ? 'red' : 'green'; ?>;">
+                        <?= $promotion['promo_expiration']; ?>
+                    </td>
+
                     <td class="p-2">
                         <button class="bg-blue-500 text-white py-1 px-3 rounded-md togglerUpdateUser" data-user_id="<?= $promotion['promo_id'] ?>">Update</button>
                         <button class="bg-red-500 text-white py-1 px-3 rounded-md" data-user_id="<?= $promotion['promo_id'] ?>">Remove</button>
                     </td>
                 </tr>
             <?php endforeach; ?>
-        
 <?php else: ?>
     <tr>
         <td colspan="5" class="p-2">No record found.</td>

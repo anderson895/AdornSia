7c7477b98fb2ea7cafd820f2139dc6f2628bf60d
JARP <andersonandy046@gmail.com>
2024-11-24 23:41:09 +0800

    debug

--- admin/js/table_order.js
+++ admin/js/table_order.js
@@ -64,71 +64,76 @@ function displayOrders(orders) {
     });
 
     // Display the filtered orders
-    filteredOrders.forEach(function(orderItem) { 
-        var orderDate = new Date(orderItem.order_date);
-        var formattedDate = orderDate.toLocaleString('en-US', { 
-            month: 'long', 
-            day: 'numeric', 
-            year: 'numeric', 
-            hour: 'numeric', 
-            minute: 'numeric', 
-            hour12: true 
+    if (filteredOrders.length > 0) {
+        filteredOrders.forEach(function(orderItem) { 
+            var orderDate = new Date(orderItem.order_date);
+            var formattedDate = orderDate.toLocaleString('en-US', { 
+                month: 'long', 
+                day: 'numeric', 
+                year: 'numeric', 
+                hour: 'numeric', 
+                minute: 'numeric', 
+                hour12: true 
+            });
+
+            let orderRow = `
+                <tr class="border-t">
+                    <td class="px-4 py-2 text-sm text-gray-600">${orderItem.order_code}</td>
+                    <td class="px-4 py-2 text-sm text-gray-600">${orderItem.Fullname}</td>
+                    <td class="px-4 py-2 text-sm text-gray-600">${orderItem.order_date}</td>
+                    <td class="px-4 py-2 text-sm text-gray-600">${formattedDate}</td>
+                    <td class="px-4 py-2 text-sm text-gray-600">${orderItem.subtotal}</td>
+                    <td class="px-4 py-2 text-sm text-gray-600">${orderItem.vat}</td>
+                    <td class="px-4 py-2 text-sm text-gray-600">${orderItem.total}</td>
+                    <td class="px-4 py-2 text-sm text-gray-600">${orderItem.delivery_address}</td>
+                    <td class="px-4 py-2 text-sm text-gray-600">
+                        <select 
+                            class="UpdateOrderStatus text-center w-full p-2 text-white bg-blue-500 border border-blue-500 rounded-md shadow-sm appearance-none cursor-pointer focus:outline-none focus:ring-2 focus:ring-blue-300 focus:border-blue-300" 
+                            data-orderId="${orderItem.order_id}" 
+                            data-initial-status="${orderItem.order_status}">
+                            
+                            ${orderItem.order_status == "Pending" ? 
+                                '<option value="" selected>Pending</option>' +
+                                '<option value="Accept">Accept</option>' +
+                                '<option value="Canceled" ' + (orderItem.order_status == "Canceled" ? 'selected' : '') + '>Canceled</option>' 
+                            : ''}
+                            
+                            ${orderItem.order_status == "Accept" ? 
+                                `<option value="Accept" selected>Accept</option>
+                                <option value="Shipped" ${orderItem.order_status == "Shipped" ? "selected" : ""}>Shipped</option>
+                                <option value="Delivered" ${orderItem.order_status == "Delivered" ? "selected" : ""}>Delivered</option>
+                                ` : ''
+                            }
+
+                            ${["Shipped", "Delivered", "Canceled"].includes(orderItem.order_status) ? 
+                                `
+                                <option value="Shipped" ${orderItem.order_status == "Shipped" ? "selected" : ""}>Shipped</option>
+                                <option value="Delivered" ${orderItem.order_status == "Delivered" ? "selected" : ""}>Delivered</option>
+                                <option value="Canceled" ${orderItem.order_status == "Canceled" ? "selected" : ""}>Canceled</option>
+                                ` : ''
+                            }
+                        </select>
+
+                        <button 
+                            class="mt-2 w-full px-4 py-2 text-white bg-green-500 rounded-md shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300 focus:ring-offset-1"
+                            onclick="location.href='view_orders.php?orderId=${orderItem.order_id}';">
+                            View
+                        </button>
+                    </td>
+                </tr>
+            `;
+            tableBody.append(orderRow);
         });
-
-        let orderRow = `
-            <tr class="border-t">
-                <td class="px-4 py-2 text-sm text-gray-600">${orderItem.order_code}</td>
-                <td class="px-4 py-2 text-sm text-gray-600">${orderItem.Fullname}</td>
-                <td class="px-4 py-2 text-sm text-gray-600">${orderItem.order_date}</td>
-                <td class="px-4 py-2 text-sm text-gray-600">${formattedDate}</td>
-                <td class="px-4 py-2 text-sm text-gray-600">${orderItem.subtotal}</td>
-                <td class="px-4 py-2 text-sm text-gray-600">${orderItem.vat}</td>
-                <td class="px-4 py-2 text-sm text-gray-600">${orderItem.total}</td>
-                <td class="px-4 py-2 text-sm text-gray-600">${orderItem.delivery_address}</td>
-                <td class="px-4 py-2 text-sm text-gray-600">
-                   <select 
-                        class="UpdateOrderStatus text-center w-full p-2 text-white bg-blue-500 border border-blue-500 rounded-md shadow-sm appearance-none cursor-pointer focus:outline-none focus:ring-2 focus:ring-blue-300 focus:border-blue-300" 
-                        data-orderId="${orderItem.order_id}" 
-                        data-initial-status="${orderItem.order_status}">
-                        
-                        ${orderItem.order_status == "Pending" ? 
-                            '<option value="" selected>Pending</option>' +
-                            '<option value="Accept">Accept</option>' +
-                            '<option value="Canceled" ' + (orderItem.order_status == "Canceled" ? 'selected' : '') + '>Canceled</option>' 
-                        : ''}
-                        
-
-                        ${orderItem.order_status == "Accept" ? 
-                            `<option value="Accept" selected>Accept</option>
-                            <option value="Shipped" ${orderItem.order_status == "Shipped" ? "selected" : ""}>Shipped</option>
-                            <option value="Delivered" ${orderItem.order_status == "Delivered" ? "selected" : ""}>Delivered</option>
-                            ` : ''
-                        }
-
-                        ${["Shipped", "Delivered", "Canceled"].includes(orderItem.order_status) ? 
-                            `
-                            <option value="Shipped" ${orderItem.order_status == "Shipped" ? "selected" : ""}>Shipped</option>
-                            <option value="Delivered" ${orderItem.order_status == "Delivered" ? "selected" : ""}>Delivered</option>
-                            <option value="Canceled" ${orderItem.order_status == "Canceled" ? "selected" : ""}>Canceled</option>
-                            ` : ''
-                        }
-                    </select>
-
-                   <button 
-                        class="mt-2 w-full px-4 py-2 text-white bg-green-500 rounded-md shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300 focus:ring-offset-1"
-                        onclick="location.href='view_orders.php?orderId=${orderItem.order_id}';">
-                        View
-                    </button>
-
+    } else {
+        // If no records are found
+        tableBody.append(`
+            <tr>
+                <td colspan="9" class="px-4 py-2 text-center text-sm text-gray-600">
+                    No records found
                 </td>
-
-
-
-
             </tr>
-        `;
-        tableBody.append(orderRow);
-    });
+        `);
+    }
 }
 
 

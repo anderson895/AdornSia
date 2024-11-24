e361896317d6abf84378393ab832007dc7c677eb
JARP <andersonandy046@gmail.com>
2024-11-24 14:06:13 +0800

    marketing  promotional

--- admin/backend/end-points/promotional_list.php
+++ admin/backend/end-points/promotional_list.php
@@ -13,8 +13,8 @@ if ($fetch_all_promotion): ?>
             <td class="p-2"><?= ($promotion['promo_rate']*100)/2; ?>%</td>
             <td class="p-2"><?= $promotion['promo_expiration']; ?></td>
             <td class="p-2">
-                <button class="bg-blue-500 text-white py-1 px-3 togglerUpdateUser" data-user_id=<?=$promotion['promo_id']?>>Update</button>
-                <button class="bg-red-500 text-white py-1 px-3 " data-user_id=<?=$promotion['promo_id']?>>Remove</button>
+                <button class="bg-blue-500 text-white py-1 px-3 rounded-md togglerUpdateUser" data-user_id=<?=$promotion['promo_id']?>>Update</button>
+                <button class="bg-red-500 text-white py-1 px-3 rounded-md" data-user_id=<?=$promotion['promo_id']?>>Remove</button>
             </td>
         </tr>
     <?php endforeach; ?>

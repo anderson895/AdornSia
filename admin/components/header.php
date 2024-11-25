49e0bfd3105a4776bb9e7f79d2c39589a409a10e
JARP <andersonandy046@gmail.com>
2024-11-25 20:56:51 +0800

    cart UI

--- admin/components/header.php
+++ admin/components/header.php
@@ -87,7 +87,7 @@ if (isset($_SESSION['admin_id'])) {
           <span>Reports</span>
       </a>
 
-      <a href="{{url_for('admin_student_progress')}}" class="flex items-center lg:justify-start  space-x-3 text-gray-600 hover:text-blue-500 hover:bg-gray-100 px-4 py-2 rounded-md transition-all duration-300">
+      <a href="user_role.php" class="flex items-center lg:justify-start  space-x-3 text-gray-600 hover:text-blue-500 hover:bg-gray-100 px-4 py-2 rounded-md transition-all duration-300">
             <span class="material-icons">admin_panel_settings</span>
 
           <span>User Role</span>

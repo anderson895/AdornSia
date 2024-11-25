d812bdd42e37149fadac0a39d6fbc214bd734ef9
JARP <andersonandy046@gmail.com>
2024-11-25 21:22:06 +0800

    user role

--- admin/user_role.php
+++ admin/user_role.php
@@ -3,7 +3,7 @@ include "components/header.php";
 ?>
 
 <div class="flex justify-between items-center bg-white p-4 mb-6 rounded-md shadow-md">
-    <h2 class="text-lg font-semibold text-gray-700">User Role</h2>
+    <h2 class="text-lg font-semibold text-gray-700">Admin</h2>
     <div class="w-10 h-10 bg-gray-300 rounded-full flex items-center justify-center text-lg font-bold text-white">
         <?php
         echo substr(ucfirst($_SESSION['admin_username']), 0, 1);
@@ -13,7 +13,6 @@ include "components/header.php";
 
 <!-- Card for Table -->
 <div class="bg-white rounded-lg shadow-lg p-6">
-    <h3 class="text-xl font-semibold text-gray-700 mb-4">User List</h3>
 
     <!-- Table Wrapper for Responsiveness -->
     <div class="overflow-x-auto">

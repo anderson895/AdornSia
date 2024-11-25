00c254f202ff82e45e04b21d6edd2abb2e39a74e
JARP <andersonandy046@gmail.com>
2024-11-25 23:45:50 +0800

    Add logs

--- admin/backend/end-points/activity_log_list.php
+++ admin/backend/end-points/activity_log_list.php
@@ -4,8 +4,8 @@ $fetch_all_activity = $db->fetch_all_activity();
 
 if ($fetch_all_activity): ?>
     <?php foreach ($fetch_all_activity as $log): 
-        // Format the date and time (example: "Y-m-d H:i:s" for "2024-11-25 14:30:00")
-        $formatted_date = (new DateTime($log['log_date']))->format('Y-m-d H:i:s');
+        // Format the date and time to a more readable format (e.g., "November 25, 2024 at 2:30 PM")
+        $formatted_date = (new DateTime($log['log_date']))->format('F j, Y \a\t g:i A');
         ?>
         <tr>
             <td class="p-2"><?= $log['log_id']; ?></td>

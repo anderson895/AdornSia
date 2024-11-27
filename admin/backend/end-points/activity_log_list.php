05150e981fe8dce71f03a570484a0f4bfc6da12d
JARP <andersonandy046@gmail.com>
2024-11-27 16:12:36 +0800

    https://adornsia.shop/admin/user_role.php

--- admin/backend/end-points/activity_log_list.php
+++ admin/backend/end-points/activity_log_list.php
@@ -2,21 +2,22 @@
 
 $fetch_all_activity = $db->fetch_all_activity();
 
+function formatDate($date) {
+    return (new DateTime($date))->format('F j, Y \a\t g:i A');
+}
+
 if ($fetch_all_activity): ?>
-    <?php foreach ($fetch_all_activity as $log): 
-        // Format the date and time to a more readable format (e.g., "November 25, 2024 at 2:30 PM")
-        $formatted_date = (new DateTime($log['log_date']))->format('F j, Y \a\t g:i A');
-        ?>
-        <tr>
-            <td class="p-2"><?= $log['log_id']; ?></td>
-            <td class="p-2"><?= $log['log_name']; ?></td>
-            <td class="p-2"><?= $log['log_role']; ?></td>
-            <td class="p-2"><?= $formatted_date; ?></td>
-            <td class="p-2"><?= $log['log_activity']; ?></td>
+    <?php foreach ($fetch_all_activity as $log): ?>
+        <tr class="border-b">
+            <td class="p-2"><?= htmlspecialchars($log['log_id']); ?></td>
+            <td class="p-2"><?= htmlspecialchars($log['log_name']); ?></td>
+            <td class="p-2"><?= htmlspecialchars($log['log_role']); ?></td>
+            <td class="p-2"><?= formatDate($log['log_date']); ?></td>
+            <td class="p-2"><?= htmlspecialchars($log['log_activity']); ?></td>
         </tr>
     <?php endforeach; ?>
 <?php else: ?>
     <tr>
-        <td colspan="5" class="p-2">No record found.</td>
+        <td colspan="5" class="p-2 text-center">No record found.</td>
     </tr>
 <?php endif; ?>

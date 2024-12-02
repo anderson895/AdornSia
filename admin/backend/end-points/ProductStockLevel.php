f3660b84de9fc82fa55f10a444e61bb52a9c57dc
JARP <andersonandy046@gmail.com>
2024-12-02 10:52:13 +0800

    Update ProductStockLevel.php

--- admin/backend/end-points/ProductStockLevel.php
+++ admin/backend/end-points/ProductStockLevel.php
@@ -7,7 +7,7 @@ $orders = $db->StockLevel();
 
 if ($orders) {
     $response = "
-    <h3 class='text-center text-gray-700 font-semibold text-lg mb-4'>Inventory Status (Low Stock)</h3>
+    <h3 class='text-center text-gray-700 font-semibold text-lg mb-4'>Inventory Status</h3>
     <ul class='space-y-4'>"; 
  
     foreach ($orders as $order) {
